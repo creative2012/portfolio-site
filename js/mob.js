78ae76b6103e1f764d612e5a0ddead9a993de817
Paul Morris <creative2012@hotmail.com>
2023-02-16 23:14:10 +0000

    testing mob height fix

--- js/mob.js
+++ js/mob.js
@@ -3,6 +3,7 @@
 const isMobile = () => {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 
-        document.getElementById('landingPageContainer').style.height = '100%';
+        window.scrollTop = 10;
+        window.scrollTop = scroll;
     }
 }

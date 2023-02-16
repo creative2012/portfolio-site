7b13cbd93b9360fbb58b5899b0c55ad27ac45631
Paul Morris <creative2012@hotmail.com>
2023-02-16 20:39:53 +0000

    added more split elements to landing page exit

--- js/script.js
+++ js/script.js
@@ -1,48 +1,77 @@
 const one = document.getElementById('one');
 const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const ten = document.getElementById('ten');
 const welcomeText = document.getElementById('welcomeText');
 const about = document.getElementById('about');
 const nav = document.querySelector('nav');
-const mainTitle = document.getElementById('mainTitle')
+const mainTitle = document.getElementById('mainTitle');
+
+const screenArrayTop = [five, three, seven, one, nine];
+const screenArrayBottom = [six, four, eight, two, ten];
 
 // one.style.width = "50%";
 // two.style.width = "50%";
 setTimeout(() => {
     welcomeText.style.opacity = "1";
+}, 200);
 
-},200);
-setTimeout(() =>{
+setTimeout(() => {
     mainTitle.style.transform = "translateX(0)";
-},500);
+}, 500);
+
+function rotate(event) {
+    let midpoint = window.innerWidth / 2;
+    let pos = event.clientX - midpoint;
+    let pos2 = event.clientY - midpoint;
+    let val = (pos / midpoint) * 2;
+    let val2 = (pos2 / midpoint) * 10;
+    welcomeText.style.transform = "perspective(550px) rotateY(" + val + "deg) rotateX(" + val2 + "deg)";
+}
+
+document.addEventListener("mousemove", rotate, false);
 
 about.addEventListener('click', () => {
+    document.removeEventListener("mousemove", rotate, false);
     welcomeText.style.transition = '0.5s';
     welcomeText.style.transform = "translate(20px, 0) scale(0.9)";
-    setTimeout(() =>{
-        
-        // nav.style.backgroundColor = "transparent";
-        // nav.style.fontWeight = "bold";
+
+    setTimeout(() => {
         welcomeText.style.transition = '1s';
         welcomeText.style.transform = "translate(-200vw, 0) scale(0.9)";
-    },500)
-    // setTimeout(() =>{
-    // one.style.backgroundColor = "white";
-    // two.style.backgroundColor = "white";
-    // },700);
-    setTimeout(() =>{
-        one.style.top = "0";
-        two.style.bottom = "0";
-        one.style.height = "0%";
-        two.style.height = "0%";
-        // nav.style.color = "black";
-    },800);
+    }, 500);
+
+    let timer = 800;
+    let timer2 = 800;
+    screenArrayTop.forEach((item) => 
+        {
+            setTimeout(() => {
+        
+                item.style.height = "0%";
+            }, timer);
+            timer += 100;
+
+        });
+        screenArrayBottom.forEach((item) => 
+        {
+            setTimeout(() => {
+        
+                item.style.height = "0%";
+            }, timer2);
+            timer2 += 100;
+
+        });
+
+    // setTimeout(() => {
+        
+    //     one.style.height = "0%";
+    //     two.style.height = "0%";
+    // }, 800);
 })
 
-// setTimeout(() =>{
-//     one.style.backgroundColor = "red";
-//     two.style.backgroundColor = "black";
-//     one.style.top = "0";
-//     two.style.bottom = "0";
-//     one.style.height = "0%";
-//     two.style.height = "0%";
-// },2000);

2a0881b7305a63a38223d83e0011da4dcf3f7c33
Paul Morris <creative2012@hotmail.com>
2023-02-16 23:35:09 +0000

    removed mob fix

--- js/mob.js
+++
@@ -1,9 +0,0 @@
-
-//is mobile?
-const isMobile = () => {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        window.scrollTop = 10;
-        window.scrollTop = scroll;
-        
-    }
-}

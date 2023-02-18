befc7486f526b7e22f1b74596431f3178e9fc744
Paul Morris <creative2012@hotmail.com>
2023-02-18 18:59:49 +0000

    mob resize fix

--- js/logic.js
+++ js/logic.js
@@ -7,8 +7,14 @@ const aboutPage = new AboutPage();
 interface.init();
 landingPage.init();
 aboutPage.init();
+window.addEventListener("resize", onResize);
+function onResize(){
+this.height = window.innerHeight - 30;
 
-
+        document.querySelector("body").style.height = this.height + "px";
+        window.scrollTo(0, 0);
+        document.body.scrollTop = 0;
+}
 
 
 
